5536f4d8dfdf830932c4978a8d71bccb814ef0be
Bálint Berente <balint.berente@gmail.com>
2022-03-15 20:23:50 +0100

    Messenger auth and save

--- src/router.ts
+++ src/router.ts
@@ -6,12 +6,23 @@ import parseAlertsMW from "./mw/parseAlertsMW";
 import getMessagesMW from "./mw/getMessagesMW";
 import addMessageMW from "./mw/addMessageMW";
 import removeMessageMW from "./mw/removeMessageMW";
+import messengerAuthMW from "./mw/messengerAuthMW";
 
 export function Router(app: Express) {
   app.post("/api", getDataMW(), parseResponseMW(), apiMW());
   app.post("/api/alerts", getDataMW(), parseAlertsMW(), apiMW());
   app.get("/api/messages", getMessagesMW(), apiMW());
-  app.put("/api/messages", addMessageMW(), apiMW(true));
-  app.delete("/api/messages/:id", removeMessageMW(), apiMW(true));
-  app.delete("/api/messages/", removeMessageMW(true), apiMW(true));
+  app.put("/api/messages", messengerAuthMW(), addMessageMW(), apiMW(true));
+  app.delete(
+    "/api/messages/:id",
+    messengerAuthMW(),
+    removeMessageMW(),
+    apiMW(true)
+  );
+  app.delete(
+    "/api/messages/",
+    messengerAuthMW(),
+    removeMessageMW(true),
+    apiMW(true)
+  );
 }
--- src/types/messenger.ts
+++ src/types/messenger.ts
@@ -1,17 +1,29 @@
+import { readFileSync, writeFile } from "fs";
+
+const fileName = "messages.json";
+
 export class Messenger {
-  private messages: MessageWithId[] = [];
-  private lastId = 1;
+  private messages: MessageWithId[];
+  private lastId;
+
+  constructor() {
+    this.messages = loadMessages();
+    this.lastId = getLastId(this.messages);
+  }
   addMessage(message: Message) {
     this.messages.push({ id: this.lastId++, ...message });
+    saveMessages(this.messages);
   }
   clearMessages() {
-    return (this.messages = []);
+    saveMessages(this.messages);
+    this.messages = [];
   }
   getMessages() {
     return this.messages;
   }
   removeMessage(id: number) {
     this.messages = this.messages.filter((m) => m.id !== id);
+    saveMessages(this.messages);
   }
 }
 
@@ -30,3 +42,27 @@ export type Message = {
 export type MessageWithId = {
   id: number;
 } & Message;
+
+function saveMessages(messages: MessageWithId[]) {
+  writeFile(fileName, JSON.stringify(messages), (errno) => {
+    if (errno) console.error("Üzenetek mentése sikertelen.");
+  });
+}
+
+function loadMessages() {
+  try {
+    const loaded = readFileSync(fileName).toString();
+    return JSON.parse(loaded) as MessageWithId[];
+  } catch (err) {
+    console.error("Üzenetek betöltése sikertelen.");
+    return [] as MessageWithId[];
+  }
+}
+
+function getLastId(messages: MessageWithId[]) {
+  let highest = 0;
+  for (let m of messages) {
+    if (m.id > highest) highest = m.id;
+  }
+  return highest + 1;
+}
